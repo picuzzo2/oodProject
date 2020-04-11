5acb3430cf0331491216281b53b0665c9823a85e
Anicha Harnpa <earn.anicha@gmail.com>
2020-04-11 17:08:59 +0700

    home+notification in company user

--- CSFinder/Controllers/CompanyController.cs
+++ CSFinder/Controllers/CompanyController.cs
@@ -15,16 +15,47 @@ namespace CSFinder.Controllers
         }
         public IActionResult Home()
         {
-            ViewBag.companyName = "บริษัท ปูนซีเมนต์ไทย จำกัด (มหาชน)";
-            ViewBag.companyAddress = "เลขที่ 1 ถนนปูนซีเมนต์ไทย แขวงบางซื่อ เขตบางซื่อ กรุงเทพฯ 10800";
-            ViewBag.companyPhone = "053-222180";
-            ViewBag.companyLine = "@scg.contact.center";
+            ViewBag.companyName = "บริษัท เอ็ม เอฟ อี ซี จำกัด (มหาชน)";
+            ViewBag.companyAddress = "699 อาคารโมเดอร์นฟอร์มทาวเวอร์ ชั้น 27 ถนนศรีนครินทร์ แขวงพัฒนาการ เขตสวนหลวง กรุงเทพมหานคร 10250";
+            ViewBag.companyPhone = "+66 (0) 2821-7999";
+            ViewBag.companyEmail = "sales@mfec.com";
             ViewBag.companyPostName = "บริษัท HandyWings";
             ViewBag.companyPostDetail = "บริษัท HandyWings เปิดรับสมัคร เพื่อนร่วมทีม เพิ่มเติมหลายอัตราเพื่อรองรับการขยายงาน";
+            ViewBag.ProfileOtherCompanyPost = "https://sv1.picz.in.th/images/2020/04/10/QMv1uZ.jpg";
+            ViewBag.Post1Img = "https://sv1.picz.in.th/images/2020/04/11/UWg8eR.jpg";
+            ViewBag.PhotoOfOtherCompanyPost = "https://sv1.picz.in.th/images/2020/04/10/QMvKII.jpg";
             return View();
         }
         public IActionResult Notification()
+
         {
+            ViewBag.companyName = "บริษัท เอ็ม เอฟ อี ซี จำกัด (มหาชน)";
+            ViewBag.companyAddress = "699 อาคารโมเดอร์นฟอร์มทาวเวอร์ ชั้น 27 ถนนศรีนครินทร์ แขวงพัฒนาการ เขตสวนหลวง กรุงเทพมหานคร 10250";
+            ViewBag.companyPhone = "+66 (0) 2821-7999";
+            ViewBag.companyEmail = "sales@mfec.com";
+            ViewBag.Post1Img = "https://sv1.picz.in.th/images/2020/04/11/UWg8eR.jpg";
+            ViewBag.studentPhotoexample = "https://www.w3schools.com/howto/img_avatar.png";
+            ViewBag.studentInterestName1 = "นางสาวอณิชา หารป่า";
+            ViewBag.studentInterestName2 = "นางสาวปานระวี ไชยสิทธิ์";
+            ViewBag.studentInterestName3 = "นายภรัญยู วงศ์แสง";
+            return View();
+        }
+
+        public IActionResult Notification_Announcement()
+
+        {
+            ViewBag.companyName = "บริษัท เอ็ม เอฟ อี ซี จำกัด (มหาชน)";
+            ViewBag.companyAddress = "699 อาคารโมเดอร์นฟอร์มทาวเวอร์ ชั้น 27 ถนนศรีนครินทร์ แขวงพัฒนาการ เขตสวนหลวง กรุงเทพมหานคร 10250";
+            ViewBag.companyPhone = "+66 (0) 2821-7999";
+            ViewBag.companyEmail = "sales@mfec.com";
+            ViewBag.Post1Img = "https://sv1.picz.in.th/images/2020/04/11/UWg8eR.jpg";
+            ViewBag.studentPhotoexample = "https://www.w3schools.com/howto/img_avatar.png";
+            ViewBag.studentInterestName1 = "นางสาวอณิชา หารป่า";
+            ViewBag.studentInterestName2 = "นางสาวปานระวี ไชยสิทธิ์";
+            ViewBag.studentInterestName3 = "นายภรัญยู วงศ์แสง";
+            ViewBag.studentInterestStatus1 = "รอสัมภาษณ์";
+            ViewBag.studentInterestStatus2 = "ผ่าน";
+            ViewBag.studentInterestStatus3 = "ไม่ผ่าน";
             return View();
         }
         public IActionResult Logout()
